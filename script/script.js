ec49b36b2c6957ad7086e873da2e7cea5b3795cc
DaniilBenyash <daniilbenyash22@gmail.com>
2022-01-12 18:12:36 +0300

    add  new pokemons

--- script/script.js
+++ script/script.js
@@ -1,68 +1,41 @@
 const section = document.querySelector('.section');
 const pokemon = document.querySelector('.pokemon');
-const addPokemon = document.querySelector('.addPokemon')
+const addPokemon = document.querySelector('.addPokemon');
 
 class CreatePokemon {
     constructor(name,image,health,hunger,hygiene,depression){
-       const newPokemon = pokemon.cloneNode(true);
+        this.name = name;
+        this.image = image;
+        this.health = health;
+        this.hunger = hunger;
+        this.hygiene = hygiene;
+        this.depression = depression;  
+    }
+    methodCreatePokemon(){
+        const newPokemon = pokemon.cloneNode(true);
        section.append(newPokemon);
-    
-       newPokemon.childNodes[3].innerText = name;
-       newPokemon.childNodes[1].style.backgroundImage = image;
+        
+       newPokemon.childNodes[3].innerText = this.name;
+       newPokemon.childNodes[1].style.backgroundImage = this.image;
 
        const character = newPokemon.childNodes[5];
 
        const hp = character.childNodes[1];
-       hp.childNodes[3].value = health;
-    
+       hp.childNodes[3].max = this.health;
+
        const hr = character.childNodes[3];
-       hr.childNodes[3].value = hunger;
+       hr.childNodes[3].max = this.hunger;
         
        const he = character.childNodes[5];
-       he.childNodes[3].value = hygiene;
+       he.childNodes[3].max = this.hygiene;
 
        const dn = character.childNodes[7];
-       dn.childNodes[3].value = depression;
+       dn.childNodes[3].max = this.depression;
     }
-}
-
-const Psyduck = {
-    image: "url('../image/psyduck.png')",
-    name: 'Псайдак',
-    health: 100,
-    hunger: 100,
-    hygiene: 100,
-    depression: 100,
-}
-const Bulbasaur = {
-    image: "url('../image/bulbasaur.png')",
-    name: 'Бульбазавр',
-    health: 150,
-    hunger: 150,
-    hygiene: 150,
-    depression: 150,
-}
-const Charmander = {
-    image: "url('../image/charmander.png')",
-    name: 'Чармандер',
-    health: 160,
-    hunger: 140,
-    hygiene: 120,
-    depression: 110,
-}
-const Squirtle = {
-    image: "url('../image/squirtle.png')",
-    name: 'Сквиртл',
-    health: 140,
-    hunger: 130,
-    hygiene: 100,
-    depression: 120,
-}
-
-const arrPokemons = [];
-arrPokemons.push(Psyduck,Bulbasaur,Charmander,Squirtle)
+};
 
 addPokemon.addEventListener('click',() => {
-    const random = Math.floor(Math.random() * arrPokemons.length)
-    const create = new CreatePokemon(arrPokemons[random].name,arrPokemons[random].image,arrPokemons[random].health,arrPokemons[random].hunger,arrPokemons[random].hygiene,arrPokemons[random].depression)
+    const random = Math.floor(Math.random() * arrPokemons.length);
+    const create = new CreatePokemon(arrPokemons[random].name,arrPokemons[random].image,arrPokemons[random].health,arrPokemons[random].hunger,arrPokemons[random].hygiene,arrPokemons[random].depression);
+    create.methodCreatePokemon();
 })
