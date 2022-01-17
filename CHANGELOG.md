08add9c3b516b093297312bda9c65ac599ea05f8
DaniilBenyash <93876781+DaniilBenyash@users.noreply.github.com>
2022-01-17 17:02:11 +0300

    Add files via upload

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # Changelog
 Все заметные изменения в этом проекте будут задокументированы в этом файле.
 
@@ -23,18 +22,3 @@
 ## [0.1.0] - 2021.01.12
 ### Added
 - Функционал создания и отображения карточки покемона.
-=======
-## [0.3.0](https://github.com/DaniilBenyash/DZ_12_Pokemon/commit/bc7642c086c3bc87170e158fb993c85f8e088dcb) - 2021.01.13
-### Changed
-- Цвет фона покемона.
-### Fixed
-- Адаптивность сайта.
-
-## [0.2.0](https://github.com/DaniilBenyash/DZ_12_Pokemon/commit/ec49b36b2c6957ad7086e873da2e7cea5b3795cc) - 2021.01.12
-### Added
-- Новые покемоны.
-
-## [0.1.0](https://github.com/DaniilBenyash/DZ_12_Pokemon/commit/f3bc8515f9079a2bb983d0f60bade7301dd16f9d) - 2021.01.12
-### Added
-- Функционал создания и отображения карточки покемона.
->>>>>>> e7e02a440bbfa57955c79237a2ff76326858058f
--- script/script.js
+++ script/script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 class CreatePokemon {
     constructor(name,image,health,hunger,hygiene,depression){
         this.name = name;
@@ -97,51 +96,4 @@ document.querySelector('.addPokemon').addEventListener('click',() => {
         arrPokemons[random].hygiene,
         arrPokemons[random].depression,
         );
-=======
-const section = document.querySelector('.section');
-const pokemon = document.querySelector('.pokemon');
-const addPokemon = document.querySelector('.addPokemon');
-
-class CreatePokemon {
-    constructor(name,image,health,hunger,hygiene,depression){
-        this.name = name;
-        this.image = image;
-        this.health = health;
-        this.hunger = hunger;
-        this.hygiene = hygiene;
-        this.depression = depression;  
-    }
-    methodCreatePokemon(){
-        const newPokemon = pokemon.cloneNode(true);
-        section.append(newPokemon);
-        
-        newPokemon.childNodes[3].innerText = this.name;
-        newPokemon.childNodes[1].style.backgroundImage = this.image;
-
-        const character = newPokemon.childNodes[5];
-
-        const hp = character.childNodes[1];
-        hp.childNodes[3].max = this.health;
-        hp.childNodes[3].value = this.health;
-
-        const hr = character.childNodes[3];
-        hr.childNodes[3].max = this.hunger;
-        hr.childNodes[3].value = this.hunger;
-        
-        const he = character.childNodes[5];
-        he.childNodes[3].max = this.hygiene;
-        he.childNodes[3].value = this.hygiene;
-
-        const dn = character.childNodes[7];
-        dn.childNodes[3].max = this.depression;
-        dn.childNodes[3].value = this.depression;
-       
-    }
-};
-
-addPokemon.addEventListener('click',() => {
-    const random = Math.floor(Math.random() * arrPokemons.length);
-    const create = new CreatePokemon(arrPokemons[random].name,arrPokemons[random].image,arrPokemons[random].health,arrPokemons[random].hunger,arrPokemons[random].hygiene,arrPokemons[random].depression);
-    create.methodCreatePokemon();
->>>>>>> e7e02a440bbfa57955c79237a2ff76326858058f
 })
